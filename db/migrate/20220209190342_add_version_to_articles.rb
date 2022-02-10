1704559d9ee1d967e0faf945accce3bb66180ca8
Hathus <trilogyti@gmail.com>
2022-02-10 09:38:37 -0600

    Remove version column from articles table

--- db/migrate/20220209190342_add_version_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddVersionToArticles < ActiveRecord::Migration[7.0]
-  def change
-    add_column :articles, :version, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_190342) do
+ActiveRecord::Schema.define(version: 2022_02_09_181959) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "version"
   end
 
 end
